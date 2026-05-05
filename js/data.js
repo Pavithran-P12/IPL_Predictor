9ece680fdc077f74c297522ccf54cf0ac81995cd
Pavithran M <pavi.12.pavi@gmail.com>
2026-05-06 00:11:31 +0530

    data
    data

--- js/data.js
+++ js/data.js
@@ -13,15 +13,15 @@ const TeamMeta = {
     LSG: { name: 'Lucknow Super Giants', short: 'LSG', color: '#004f91', gradient: 'linear-gradient(135deg, #004f91, #a72056)' }
 };
 
-// Current standings as of May 3, 2026
+// Current standings as of May 5, 2026
 const Standings = [
     { team: 'PBKS', p: 9, w: 6, l: 2, nr: 1, pts: 13, nrr: 0.855 },
     { team: 'RCB', p: 9, w: 6, l: 3, nr: 0, pts: 12, nrr: 1.420 },
     { team: 'SRH', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: 0.644 },
     { team: 'RR', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: 0.510 },
     { team: 'GT', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: -0.147 },
-    { team: 'CSK', p: 9, w: 4, l: 5, nr: 0, pts: 8, nrr: 0.005 },
-    { team: 'DC', p: 9, w: 4, l: 5, nr: 0, pts: 8, nrr: -0.895 },
+    { team: 'CSK', p: 10, w: 5, l: 5, nr: 0, pts: 10, nrr: 0.151 },
+    { team: 'DC', p: 10, w: 4, l: 6, nr: 0, pts: 8, nrr: -0.949 },
     { team: 'KKR', p: 9, w: 3, l: 5, nr: 1, pts: 7, nrr: -0.539 },
     { team: 'MI', p: 10, w: 3, l: 7, nr: 0, pts: 6, nrr: -0.649 },
     { team: 'LSG', p: 9, w: 2, l: 7, nr: 0, pts: 4, nrr: -1.706 }
@@ -34,8 +34,8 @@ const RecentForm = {
     SRH: ['L', 'W', 'W', 'W', 'W'],
     RR: ['L', 'W', 'L', 'W', 'L'],
     GT: ['W', 'W', 'W', 'L', 'W'],
-    DC: ['W', 'L', 'L', 'W', 'L'],
-    CSK: ['W', 'L', 'W', 'L', 'W'],
+    DC: ['L', 'W', 'L', 'L', 'W'],
+    CSK: ['W', 'W', 'L', 'W', 'L'],
     KKR: ['W', 'W', 'W', 'L', 'L'],
     MI: ['W', 'L', 'L', 'L', 'W'],
     LSG: ['L', 'L', 'L', 'L', 'L']
@@ -43,7 +43,6 @@ const RecentForm = {
 
 // Remaining matches (Match 47 onwards)
 const RemainingMatches = [
-    { id: 48, home: 'DC', away: 'CSK', date: 'May 5', venue: 'Delhi' },
     { id: 49, home: 'SRH', away: 'PBKS', date: 'May 6', venue: 'Hyderabad' },
     { id: 50, home: 'LSG', away: 'RCB', date: 'May 7', venue: 'Lucknow' },
     { id: 51, home: 'DC', away: 'KKR', date: 'May 8', venue: 'Delhi' },

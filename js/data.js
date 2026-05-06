ce7062c87bf43fed27fe302a549ac63c69983790
Pavithran M <pavi.12.pavi@gmail.com>
2026-05-07 00:16:12 +0530

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -13,11 +13,11 @@ const TeamMeta = {
     LSG: { name: 'Lucknow Super Giants', short: 'LSG', color: '#004f91', gradient: 'linear-gradient(135deg, #004f91, #a72056)' }
 };
 
-// Current standings as of May 5, 2026
+// Current standings as of May 6, 2026
 const Standings = [
-    { team: 'PBKS', p: 9, w: 6, l: 2, nr: 1, pts: 13, nrr: 0.855 },
+    { team: 'SRH', p: 11, w: 7, l: 4, nr: 0, pts: 14, nrr: 0.737 },
+    { team: 'PBKS', p: 10, w: 6, l: 3, nr: 1, pts: 13, nrr: 0.571 },
     { team: 'RCB', p: 9, w: 6, l: 3, nr: 0, pts: 12, nrr: 1.420 },
-    { team: 'SRH', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: 0.644 },
     { team: 'RR', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: 0.510 },
     { team: 'GT', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: -0.147 },
     { team: 'CSK', p: 10, w: 5, l: 5, nr: 0, pts: 10, nrr: 0.151 },
@@ -29,9 +29,9 @@ const Standings = [
 
 // Recent form (last 5 matches, most recent first)
 const RecentForm = {
-    PBKS: ['L', 'L', 'W', 'W', 'W'],
+    PBKS: ['L', 'L', 'L', 'W', 'W'],
     RCB: ['W', 'W', 'W', 'W', 'L'],
-    SRH: ['L', 'W', 'W', 'W', 'W'],
+    SRH: ['W', 'L', 'W', 'W', 'W'],
     RR: ['L', 'W', 'L', 'W', 'L'],
     GT: ['W', 'W', 'W', 'L', 'W'],
     DC: ['L', 'W', 'L', 'L', 'W'],
@@ -43,7 +43,6 @@ const RecentForm = {
 
 // Remaining matches (Match 47 onwards)
 const RemainingMatches = [
-    { id: 49, home: 'SRH', away: 'PBKS', date: 'May 6', venue: 'Hyderabad' },
     { id: 50, home: 'LSG', away: 'RCB', date: 'May 7', venue: 'Lucknow' },
     { id: 51, home: 'DC', away: 'KKR', date: 'May 8', venue: 'Delhi' },
     { id: 52, home: 'RR', away: 'GT', date: 'May 9', venue: 'Jaipur' },

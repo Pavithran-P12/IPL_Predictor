fb03e968de0184faa0dfae467c9f4bed813aaf4c
Pavithran M <pavi.12.pavi@gmail.com>
2026-05-09 00:31:45 +0530

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ const TeamMeta = {
     LSG: { name: 'Lucknow Super Giants', short: 'LSG', color: '#004f91', gradient: 'linear-gradient(135deg, #004f91, #a72056)' }
 };
 
-// Current standings as of May 7, 2026
+// Current standings as of May 9, 2026
 const Standings = [
     { team: 'SRH', p: 11, w: 7, l: 4, nr: 0, pts: 14, nrr: 0.737 },
     { team: 'PBKS', p: 10, w: 6, l: 3, nr: 1, pts: 13, nrr: 0.571 },
@@ -21,8 +21,8 @@ const Standings = [
     { team: 'RR', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: 0.510 },
     { team: 'GT', p: 10, w: 6, l: 4, nr: 0, pts: 12, nrr: -0.147 },
     { team: 'CSK', p: 10, w: 5, l: 5, nr: 0, pts: 10, nrr: 0.151 },
-    { team: 'DC', p: 10, w: 4, l: 6, nr: 0, pts: 8, nrr: -0.949 },
-    { team: 'KKR', p: 9, w: 3, l: 5, nr: 1, pts: 7, nrr: -0.539 },
+    { team: 'KKR', p: 10, w: 4, l: 5, nr: 1, pts: 9, nrr: -0.169 },
+    { team: 'DC', p: 11, w: 4, l: 7, nr: 0, pts: 8, nrr: -1.154 },
     { team: 'MI', p: 10, w: 3, l: 7, nr: 0, pts: 6, nrr: -0.649 },
     { team: 'LSG', p: 10, w: 3, l: 7, nr: 0, pts: 6, nrr: -0.934 }
 ];
@@ -30,20 +30,19 @@ const Standings = [
 // Recent form (last 5 matches, most recent first)
 const RecentForm = {
     PBKS: ['L', 'L', 'L', 'W', 'W'],
-    RCB: ['L', 'W', 'W', 'W', 'W'],
-    SRH: ['W', 'L', 'W', 'W', 'W'],
+    RCB: ['L', 'L', 'W', 'W', 'W'],
+    SRH: ['L', 'W', 'W', 'W', 'W'],
     RR: ['L', 'W', 'L', 'W', 'L'],
-    GT: ['W', 'W', 'W', 'L', 'W'],
-    DC: ['L', 'W', 'L', 'L', 'W'],
+    GT: ['W', 'W', 'W', 'L', 'L'],
+    DC: ['L', 'L', 'W', 'L', 'L'],
     CSK: ['W', 'W', 'L', 'W', 'L'],
-    KKR: ['W', 'W', 'W', 'L', 'L'],
+    KKR: ['W', 'W', 'W', 'W', 'L'],
     MI: ['W', 'L', 'L', 'L', 'W'],
     LSG: ['W', 'L', 'L', 'L', 'L']
 };
 
 // Remaining matches (Match 47 onwards)
 const RemainingMatches = [
-    { id: 51, home: 'DC', away: 'KKR', date: 'May 8', venue: 'Delhi' },
     { id: 52, home: 'RR', away: 'GT', date: 'May 9', venue: 'Jaipur' },
     { id: 53, home: 'CSK', away: 'LSG', date: 'May 10', venue: 'Chennai' },
     { id: 54, home: 'RCB', away: 'MI', date: 'May 10', venue: 'Raipur' },

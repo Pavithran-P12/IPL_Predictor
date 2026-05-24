e8c75e025072ef3d5ce072002f5aab470c9c37d9
Pavithran M <pavi.12.pavi@gmail.com>
2026-05-24 23:36:25 +0530

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -13,32 +13,32 @@ const TeamMeta = {
     LSG: { name: 'Lucknow Super Giants', short: 'LSG', color: '#004f91', gradient: 'linear-gradient(135deg, #004f91, #a72056)' }
 };
 
-// Current standings as of May 20, 2026 (after match 65)
+// Current standings (updated)
 const Standings = [
-    { team: 'RCB', p: 13, w: 9, l: 4, nr: 0, pts: 18, nrr: 1.065 },
-    { team: 'GT', p: 13, w: 8, l: 5, nr: 0, pts: 16, nrr: 0.400 },
-    { team: 'SRH', p: 13, w: 8, l: 5, nr: 0, pts: 16, nrr: 0.350 },
-    { team: 'RR', p: 13, w: 7, l: 6, nr: 0, pts: 14, nrr: 0.083 },
-    { team: 'PBKS', p: 13, w: 6, l: 6, nr: 1, pts: 13, nrr: 0.227 },
-    { team: 'KKR', p: 13, w: 6, l: 6, nr: 1, pts: 13, nrr: 0.011 },
-    { team: 'CSK', p: 13, w: 6, l: 7, nr: 0, pts: 12, nrr: -0.016 },
-    { team: 'DC', p: 13, w: 6, l: 7, nr: 0, pts: 12, nrr: -0.871 },
-    { team: 'MI', p: 13, w: 4, l: 9, nr: 0, pts: 8, nrr: -0.510 },
-    { team: 'LSG', p: 13, w: 4, l: 9, nr: 0, pts: 8, nrr: -0.702 }
+    { team: 'RCB', p: 14, w: 9, l: 5, nr: 0, pts: 18, nrr: 0.783 },
+    { team: 'GT', p: 14, w: 9, l: 5, nr: 0, pts: 18, nrr: 0.695 },
+    { team: 'SRH', p: 14, w: 9, l: 5, nr: 0, pts: 18, nrr: 0.524 },
+    { team: 'RR', p: 14, w: 8, l: 6, nr: 0, pts: 16, nrr: 0.189 },
+    { team: 'PBKS', p: 14, w: 7, l: 6, nr: 1, pts: 15, nrr: 0.309 },
+    { team: 'DC', p: 14, w: 7, l: 7, nr: 0, pts: 14, nrr: -0.651 },
+    { team: 'KKR', p: 14, w: 6, l: 7, nr: 1, pts: 13, nrr: -0.147 },
+    { team: 'CSK', p: 14, w: 6, l: 8, nr: 0, pts: 12, nrr: -0.345 },
+    { team: 'MI', p: 14, w: 4, l: 10, nr: 0, pts: 8, nrr: -0.584 },
+    { team: 'LSG', p: 14, w: 4, l: 10, nr: 0, pts: 8, nrr: -0.740 }
 ];
 
 // Recent form (last 5 matches, most recent first)
 const RecentForm = {
-    PBKS: ['L', 'L', 'L', 'L', 'L'],
-    RCB: ['W', 'W', 'W', 'W', 'L'],
-    SRH: ['W', 'L', 'L', 'W', 'W'],
-    RR: ['W', 'L', 'L', 'L', 'W'],
-    GT: ['L', 'W', 'W', 'W', 'W'],
-    DC: ['W', 'W', 'L', 'L', 'W'],
-    CSK: ['L', 'L', 'W', 'W', 'W'],
-    KKR: ['W', 'W', 'W', 'L', 'W'],
-    MI: ['L', 'W', 'L', 'W', 'L'],
-    LSG: ['L', 'W', 'L', 'W', 'L']
+    PBKS: ['W', 'L', 'L', 'L', 'L'],
+    RCB: ['L', 'W', 'W', 'W', 'W'],
+    SRH: ['W', 'W', 'L', 'L', 'W'],
+    RR: ['W', 'W', 'L', 'L', 'L'],
+    GT: ['W', 'L', 'W', 'W', 'W'],
+    DC: ['W', 'W', 'W', 'L', 'L'],
+    CSK: ['L', 'L', 'L', 'W', 'W'],
+    KKR: ['L', 'W', 'W', 'W', 'L'],
+    MI: ['L', 'L', 'W', 'L', 'W'],
+    LSG: ['L', 'L', 'W', 'L', 'W']
 };
 
 // Remaining matches (Match 66 onwards)
